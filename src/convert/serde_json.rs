5ba4dc63f837f70e89b97485541dc235b26d543d
Timothée Haudebourg <timothee.haudebourg@spruceid.com>
2023-10-05 16:09:41 +0200

    Add doc examples to `from/into_serde_json`.

--- src/convert/serde_json.rs
+++ src/convert/serde_json.rs
@@ -15,6 +15,42 @@ impl<M> Value<M> {
 	///
 	/// The function `f` is used to annotate the output each sub value
 	/// (passed as parameter).
+	///
+	/// # Example
+	///
+	/// ```
+	/// // First we create a `serde_json` value.
+	/// let a = serde_json::json!({
+	///   "foo": 1,
+	///   "bar": [2, 3]
+	/// });
+	///
+	/// // We convert the `serde_json` value into a `json_syntax` value.
+	/// let b = json_syntax::Value::from_serde_json(a, |fragment| {
+	///   // `json_syntax` keeps metadata information attached to every value
+	///   // fragment, which `serde_json` does not. This is why this closure is
+	///   // necessary. It is called for every `serde_json` fragment to let you
+	///   // choose the metadata you want to associate to the fragment.
+	///   // This is intended to store code mapping information, but you can store
+	///   // any information. Here we store a text description of the fragment.
+	///   match fragment {
+	///     json_syntax::SerdeJsonFragment::Key(key) => {
+	///       format!("I'm an object key `{key}`")
+	///     }
+	///     json_syntax::SerdeJsonFragment::Value(value) => match value {
+	///       serde_json::Value::Null => "I'm the `null` value".to_string(),
+	///       serde_json::Value::Bool(b) => format!("I'm the boolean `{b:?}`"),
+	///       serde_json::Value::Number(n) => format!("I'm the number {n}"),
+	///       serde_json::Value::String(s) => format!("I'm the string {s:?}"),
+	///       serde_json::Value::Array(a) => format!("I'm an array of {} elements", a.len()),
+	///       serde_json::Value::Object(o) => format!("I'm an object of {} entries", o.len()),
+	///     },
+	///   }
+	/// });
+	///
+	/// // We convert it back into a `serde_json` value.
+	/// let _ = json_syntax::Value::into_serde_json(b);
+	/// ```
 	pub fn from_serde_json(
 		value: serde_json::Value,
 		f: impl Clone + Fn(SerdeJsonFragment) -> M,
@@ -45,6 +81,42 @@ impl<M> Value<M> {
 	}
 
 	/// Converts a `Value` into a [`serde_json::Value`], stripping the metadata.
+	///
+	/// # Example
+	///
+	/// ```
+	/// // First we create a `serde_json` value.
+	/// let a = serde_json::json!({
+	///   "foo": 1,
+	///   "bar": [2, 3]
+	/// });
+	///
+	/// // We convert the `serde_json` value into a `json_syntax` value.
+	/// let b = json_syntax::Value::from_serde_json(a, |fragment| {
+	///   // `json_syntax` keeps metadata information attached to every value
+	///   // fragment, which `serde_json` does not. This is why this closure is
+	///   // necessary. It is called for every `serde_json` fragment to let you
+	///   // choose the metadata you want to associate to the fragment.
+	///   // This is intended to store code mapping information, but you can store
+	///   // any information. Here we store a text description of the fragment.
+	///   match fragment {
+	///     json_syntax::SerdeJsonFragment::Key(key) => {
+	///       format!("I'm an object key `{key}`")
+	///     }
+	///     json_syntax::SerdeJsonFragment::Value(value) => match value {
+	///       serde_json::Value::Null => "I'm the `null` value".to_string(),
+	///       serde_json::Value::Bool(b) => format!("I'm the boolean `{b:?}`"),
+	///       serde_json::Value::Number(n) => format!("I'm the number {n}"),
+	///       serde_json::Value::String(s) => format!("I'm the string {s:?}"),
+	///       serde_json::Value::Array(a) => format!("I'm an array of {} elements", a.len()),
+	///       serde_json::Value::Object(o) => format!("I'm an object of {} entries", o.len()),
+	///     },
+	///   }
+	/// });
+	///
+	/// // We convert it back into a `serde_json` value.
+	/// let _ = json_syntax::Value::into_serde_json(b);
+	/// ```
 	pub fn into_serde_json(Meta(this, _): Meta<Self, M>) -> serde_json::Value {
 		this.into()
 	}
